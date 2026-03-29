36b0ebd53e88241f440e874dee20430c284bc881
Mateusz Ceranka <ceranka@gmail.com>
2026-03-29 17:20:05 +0200

    Initial commit

---
+++ GitHubBrowser/GitHubBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubBrowserApp.swift
+//  GitHubBrowser
+//
+//  Created by Mateusz Ceranka on 29/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct GitHubBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
